35e4d9731b88be1c839bb74ff21a72cd17a66469
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2022-09-09 22:43:44 +0000

    [Update]課題２

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+ 
+  get'/top' => 'homes#top'
+  get 'lists/new'
+  post"lists"=>'lists#create' 
+  
+  
+  
+  
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
+
+end
